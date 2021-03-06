d37b79cebf400fa3656598c3159f19894803002f
Agnes Leclercq <agnesleclercq13@yahoo.fr>
2021-03-06 17:47:27 +0100

    création des cartes teddy sur la page d'acceuil

--- js/index.js
+++ js/index.js
@@ -1,78 +1,85 @@
+//Création des titres
+let titles = document.getElementById('presentation');
+titles.innerHTML = '<h1>Orinoco vous présente sa sélection "Oripeluche". Une collection d\'ours en peluche faits à la main.</h1>' + 
+'<h2>Faites votre choix, personnalisez votre "Teddy" et passez votre commande.</h2>';
 
+titles.style.color = "#673ab7";
+titles.style.textAlign = "center";
+
+//Création d'une fonction "getAllTeddies" pour récupérer les données de l'API
 function getAllTeddies() {
 	//Création et envoi d'une requête avec l'objet XMLHttpRequest
-let request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 
-request.onreadystatechange = function() 
-{
-	//Tant qu'il y a des images et que le statut est bon, envoi des données
-	if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-	{
-		//création d'une variable 'response' (fichier responseText)
-		let response = JSON.parse(this.responseText);
-		console.log(response);
-		teddies(response);
-	}
-};
+	request.onreadystatechange = function () {
+		//Tant qu'il y a des images et que le statut est bon, envoi des données
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			//création d'une variable 'response' (fichier responseText)
+			let response = JSON.parse(this.responseText);
+			console.log(response);
+			teddies(response);
+		}
+	};
 
-//Ouverture de l'URL avec la méthode 'GET'
-request.open('GET', 'http://localhost:3000/api/teddies');
-//Envoi de la requête au service web
-request.send();
+	//Ouverture de l'URL avec la méthode 'GET'
+	request.open('GET', 'http://localhost:3000/api/teddies');
+	//Envoi de la requête au service web
+	request.send();
 }
 
 window.onload = getAllTeddies();
 
 //document.getElementById('teddies').innerHTML = response;
 
-//Intégration d'une div dans la section id 'teddies' comprenant le nom, l'image, la description et le prix de chaque Teddy sous forme de liste
-function teddies(response)
-{
+//Récupération de la requête comprenant le nom, l'image, la description et le prix de chaque Teddy sous forme de liste
+function teddies(response) {
 	let section = document.getElementById('teddies');
-	console.log('ok')
-	for (let i = 0; i < response.length; i++) 
-	{
-		//addProduct(response[i], Teddies);
-		console.log('ok2')
-		
-		
-		section.innerHTML += 
-			"<div>" + 
-			"<h3>" + 
-			response[i].name + 
+	console.log('ok');
+
+	document.getElementById('teddies').style.border = '5px solid violet';
+
+	for (let i = 0; i < response.length; i++) {
+		console.log('ok2');
+		//Ajout d'une div et création d'un cadre autour de chaque "teddy";
+		let teddy = document.createElement('article');
+		teddy.style.textAlign = 'justify';
+		teddy.style.border = "1px solid #673ab7";
+		teddy.style.margin = "10px";
+		teddy.style.padding = "20px";
+		teddy.style.width = "300px";
+		teddy.style.height = "450px";
+		teddy.style.boxShadow = "4px 4px 4px 1px #ac96d4";
+
+		teddy._id = response[i];
+		teddy.innerHTML += "<h3>" +
+			response[i].name +
 			"</h3>" +
-			"<img width='300px' src='" + response[i].imageUrl + "'>" + 
+			"<img width='100%' src='" + response[i].imageUrl + "'>" +
 			"<p>" +
-			response[i].description + 
+			response[i].description +
 			"</p>" +
 			"<p>" +
 			response[i].price +
 			"</p>" +
-			"<a href='produit.html?id=" + response[i]._id +"'>Voir le produit</a>"
-			"</div>";
+			"<a href='produit.html?id=" + response[i]._id + "'>Voir le produit</a>"
+		section.appendChild(teddy);
+
+		/* section.innerHTML +=
+			"<div>" +
+			"<h3>" +
+			response[i].name +
+			"</h3>" +
+			"<img width='300px' src='" + response[i].imageUrl + "'>" +
+			"<p>" +
+			response[i].description +
+			"</p>" +
+			"<p>" +
+			response[i].price +
+			"</p>" +
+			"<a href='produit.html?id=" + response[i]._id + "'>Voir le produit</a>"
+		"</div>"; */
 	}
 }
-
-/*let  = new TeddyCard;
-
-class TeddyCard {
-	constructor(name, imageUrl, description, price) {
-		this.name = name;
-		this.imageUrl = imageUrl;
-		this.description = description;
-		this.price = price;
-	}
-}
-
-let teddyOne = TeddyCard(')
-*/
-
-document.getElementById('teddies').style.border = '5px solid violet';
-
-
-
-
-
 
 
 
--- js/produit.js
+++ js/produit.js
@@ -1,51 +1,61 @@
 
-function getTeddyById() {
+
+
+let header  = document.createElement('header');
+header.innerHTML = '<ul><li>Accueil</li><li>Panier</li></ul>';
+console.log('ok');
+
+//Création d'une fonction 'getTeddy' pour récupérer d'Id de chaque Teddy
+function getTeddy() {
 	//Création et envoi d'une requête avec l'objet XMLHttpRequest
-let request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 
-request.onreadystatechange = function() 
-{
-	//Tant qu'il y a des images et que le statut est bon, envoi des données
-	if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-	{
-		//création d'une variable 'response' (fichier responseText)
-		let response = JSON.parse(this.responseText);
-		console.log(response);
-		teddy(response);
-	}
-};
-
-//Ouverture de l'URL avec la méthode 'GET'
-request.open('GET', 'http://localhost:3000/api/teddies/' +id);
-//Envoi de la requête au service web
-request.send();
+	request.onreadystatechange = function () {
+		//Tant qu'il y a des images et que le statut est bon, envoi des données
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			//création d'une variable 'response' (fichier responseText)
+			let response = JSON.parse(this.responseText);
+			console.log(response);
+			teddy(response);
+		}
+	};
+	console.log('ok1');
+	//Ouverture de l'URL avec la méthode 'GET'
+	request.open('GET', 'http://localhost:3000/api/teddies/:_id');
+	//Envoi de la requête au service web
+	request.send();
 }
-console.log(request);
-
-window.onload = getTeddyById();
+console.log('ok2');
+window.onload = getTeddy();
 
 //document.getElementById('teddies').innerHTML = response;
 
 function teddy(response) {
-	let section = document.getElementById('teddy');
+	let section1 = document.getElementById('teddy');
 	console.log('ok')
+
+	document.getElementById('teddy').style.border = '5px solid violet';
+
+	let teddyProduct = document.createElement('div');
+	//teddyProduct._id = response[i];
+
 	for (let i = 0; i < response.length; i++) {
-		//addGetAllTeddies(response[i], Teddies);
+		//addteddy(response[i]);
 		console.log('ok2')
-		section.innerHTML += 
-			"<div>" + 
-			"<h3>" + 
-			response[i].name + 
+		teddyProduct.innerHTML +=
+			"<div>" +
+			"<h3>" +
+			response[i].name +
 			"</h3>" +
-			"<img src='"+response[i].imageUrl + "'>" + 
+			"<img src='" + response[i].imageUrl + "'>" +
 			"<p>" +
-			response[i].description + 
+			response[i].description +
 			"</p>" +
 			"<p>" +
-			reponse[i].colors +
+			response[i].colors +
 			"</div>";
 	}
 }
-
+section1.appendChild(teddyProduct);
 
 
